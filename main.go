4c3b5ac1a1e4be6131dcb68c1ff578fb6d46d5e6
INADA Naoki <inada-n@klab.com>
2013-05-07 13:12:07 +0900

    thread safe.

--- main.go
+++ main.go
@@ -7,69 +7,182 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // http://pusher.com/docs/auth_signatures
-var key string = "278d425bdf160c739803"
-var secret string = "7ad3773142a6692b25b8"
+var debug_key string = "278d425bdf160c739803"
+var debug_secret string = "7ad3773142a6692b25b8"
 
 type Event map[string]interface{}
+
+type App struct {
+	name     string
+	key      string
+	secret   string
+	channels map[string]*Channel
+	mutex    *sync.Mutex
+}
+
+func newApp(name string) *App {
+	return &App{
+		name,
+		debug_key,
+		debug_secret,
+		make(map[string]*Channel),
+		&sync.Mutex{},
+	}
+}
 
 type Client struct {
 	socketId string
 	buf      chan Event
+	app      *App
+	conn     *websocket.Conn
+}
+
+func newClient(app *App, conn *websocket.Conn) *Client {
+	socketId := strconv.Itoa(rand.Int())
+	log.Println("New client:", socketId)
+	client := &Client{
+		socketId,
+		make(chan Event, 16),
+		app,
+		conn}
+	go func() {
+		defer func() {
+			conn.Close()
+			if r := recover(); r != nil {
+				log.Println(r)
+			}
+		}()
+
+		for event := range client.buf {
+			err := websocket.JSON.Send(conn, event)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+		}
+	}()
+	return client
+}
+
+// Send msg to client withot blocking.
+func (client *Client) Send(msg Event) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println(r)
+		}
+	}()
+	select {
+	case client.buf <- msg:
+	default:
+		log.Println("Can't send event to", client.socketId, ". Buffer full.")
+	}
+}
+
+type AppRegistory struct {
+	apps  map[string]*App
+	mutex *sync.Mutex
+}
+
+func newAppRegistory() *AppRegistory {
+	return &AppRegistory{
+		make(map[string]*App),
+		&sync.Mutex{},
+	}
+}
+
+var appRegistory *AppRegistory = newAppRegistory()
+
+func (registory *AppRegistory) GetApp(name string) *App {
+	registory.mutex.Lock()
+	defer registory.mutex.Unlock()
+	app, ok := registory.apps[name]
+	if !ok {
+		app = newApp(name)
+		registory.apps[name] = app
+	}
+	return app
 }
 
 type Channel struct {
-	name    string
-	members map[string]*Client
-	buf     chan Event
+	name      string
+	members   map[string]*Client
+	buf       chan Event
+	subscribe chan *Client
 }
 
-func NewChannel(name string) (c *Channel) {
-	c = &Channel{
+func newChannel(name string) *Channel {
+	c := &Channel{
 		name,
 		make(map[string]*Client),
-		make(chan Event, 128),
+		make(chan Event, 16),
+		make(chan *Client),
 	}
-	go c.Run()
-	return
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println(r)
+			}
+		}()
+		for {
+			select {
+			case event, ok := <-c.buf:
+				if ok {
+					for _, client := range c.members {
+						client.Send(event)
+					}
+				} else {
+					break
+				}
+			case client := <-c.subscribe:
+				c.members[client.socketId] = client
+			}
+		}
+	}()
+	return c
+}
+
+func (app *App) GetChannel(name string) *Channel {
+	app.mutex.Lock()
+	defer app.mutex.Unlock()
+	c, _ := app.channels[name]
+	return c
+}
+
+func (app *App) Subscribe(client *Client, channelName string) {
+	app.mutex.Lock()
+	defer app.mutex.Unlock()
+
+	channel, ok := app.channels[channelName]
+	if !ok {
+		channel = newChannel(channelName)
+		app.channels[channelName] = channel
+	}
+	channel.subscribe <- client
 }
 
 func (c *Channel) Submit(event Event) {
 	c.buf <- event
 }
 
-func (c *Channel) Run() {
-	for event := range c.buf {
-		for _, client := range c.members {
-			// TODO: nonblock
-			client.buf <- event
-		}
-	}
-}
-
-var channels map[string]*Channel = make(map[string]*Channel)
-
 // TODO: thread safe
 // TODO: authorize.
 func (client *Client) Subscribe(channelName string) {
-	channel, ok := channels[channelName]
-	if !ok {
-		channel = NewChannel(channelName)
-		channels[channelName] = channel
-	}
-	channel.members[client.socketId] = client
+	client.app.Subscribe(client, channelName)
 }
 
 // TODO: thread safe
 func (client *Client) Unsubscribe(channelName string) {
-	_, ok := channels[channelName]
+	_, ok := client.app.channels[channelName]
 	if ok {
-		delete(channels, channelName)
+		delete(client.app.channels, channelName)
 	}
 }
 
+// Trigger event from client.
 func (client *Client) Trigger(event Event) {
 	eventName := event["event"].(string)
 	if !strings.HasPrefix(eventName, "client-") {
@@ -78,9 +191,10 @@ func (client *Client) Trigger(event Event) {
 		return
 	}
 	channelName := event["channel"].(string)
-	channel, ok := channels[channelName]
+	channel, ok := client.app.channels[channelName]
 	if !ok {
 		//TODO: return error.
+		log.Println("Unknown channel:", channelName)
 		return
 	}
 	channel.Submit(event)
@@ -88,50 +202,41 @@ func (client *Client) Trigger(event Event) {
 
 func GosherServer(ws *websocket.Conn) {
 	// TODO: app id etc...
-	log.Println("connected")
 	defer ws.Close()
 
-	socket_id := strconv.Itoa(rand.Int())
-	client := &Client{socket_id, make(chan Event)}
-	err := websocket.JSON.Send(ws, Event{
+	app := appRegistory.GetApp("default")
+	client := newClient(app, ws)
+	client.Send(Event{
 		"event": "pusher:connection_established",
-		"data":  Event{"socket_id": socket_id},
+		"data":  Event{"socket_id": client.socketId},
 	})
-	if err != nil {
-		log.Println(err)
-	} else {
-		go func() {
-			for event := range client.buf {
-				websocket.JSON.Send(ws, event)
-			}
-		}()
-		for {
-			var event Event
-			if err := websocket.JSON.Receive(ws, &event); err != nil {
-				log.Println(err)
-				break
-			}
-			log.Println("Received event:", event)
-			eventNameX, ok := event["event"]
-			if !ok {
-				log.Println("Unknown event:", event)
-				continue
-			}
-			eventName, ok := eventNameX.(string)
-			if !ok {
-				log.Println("Unknown event:", event)
-				continue
-			}
-			switch {
-			case eventName == "pusher:subscribe":
-				client.Subscribe(event["data"].(map[string]interface{})["channel"].(string))
-			case eventName == "pusher:unsubscribe":
-				client.Unsubscribe(event["data"].(map[string]interface{})["channel"].(string))
-			case strings.HasPrefix(eventName, "client-"):
-				client.Trigger(event)
-			default:
-				log.Println("Unknown event:", event)
-			}
+
+	for {
+		var event Event
+		if err := websocket.JSON.Receive(ws, &event); err != nil {
+			log.Println(err)
+			break
+		}
+		log.Println("Received event:", event)
+		eventNameX, ok := event["event"]
+		if !ok {
+			log.Println("Unknown event:", event)
+			continue
+		}
+		eventName, ok := eventNameX.(string)
+		if !ok {
+			log.Println("Unknown event:", event)
+			continue
+		}
+		switch {
+		case eventName == "pusher:subscribe":
+			client.Subscribe(event["data"].(map[string]interface{})["channel"].(string))
+		case eventName == "pusher:unsubscribe":
+			client.Unsubscribe(event["data"].(map[string]interface{})["channel"].(string))
+		case strings.HasPrefix(eventName, "client-"):
+			client.Trigger(event)
+		default:
+			log.Println("Unknown event:", event)
 		}
 	}
 }
